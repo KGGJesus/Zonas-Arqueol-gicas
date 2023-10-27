4da0b02a411899a95e35047e2e3e97df5361cbf3
KGGJesus <jesusbalan1232@gmail.com>
2023-10-26 19:34:52 -0600

    cambio en etiqueta html

--- resources/views/json.blade.php
+++ resources/views/json.blade.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
- <style> 
-    .titulo {
-        text-aling: center;
+    <style>
+        .titulo
+        {
+            text-align: center
     }
-</style>
+ </style>
 </head>
 <body>
-<h1 class = "titulo">Datos JSON</h1>
+<h1 class = "titulo">Datos de Zonas Arqueologicas JSON</h1>
     @if (isset($datosJson))
         <pre>{{ json_encode($datosJson, JSON_PRETTY_PRINT) }}</pre>
     @else
